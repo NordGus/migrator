90cb0a060d9a9900c8e4ccfbbdc7daecf925e763
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2019-11-29 20:38:59 +0100

    changes to how the tool works

--- main.go
+++ main.go
@@ -10,13 +10,20 @@ import (
 func main() {
 	logger := log.New(os.Stdout, "", log.LstdFlags|log.Lshortfile)
 	now := time.Now()
-	filename := fmt.Sprintf("./migrations/%v_.sql", now.Format("20060102150405"))
+	version := now.Format("20060102150405")
 
-	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	file, err := os.OpenFile("./config/migrations.go", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		logger.Println(err)
 		os.Exit(7)
 	}
-	file.Close()
+	defer file.Close()
+
+	_, err = file.WriteString(fmt.Sprintf("%v", version))
+	if err != nil {
+		logger.Println(err)
+		os.Exit(7)
+	}
+
 	logger.Println("Migration Created")
 }
